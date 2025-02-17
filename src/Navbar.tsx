c196013372e91d84dd5f99596c94d76e6615d02e
Rafael Castro <ralphc1992@live.com>
2025-02-17 00:04:41 -0600

    fixed site

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,17 +1,25 @@
 import logo from "./assets/modern-logo.svg";
+import { IoMenu } from "react-icons/io5";
 
 export const Navbar: React.FC = () => {
   return (
     <>
-      <div className="container mx-auto w-full sticky top-0 left-0 z-50 bg-white px-2 pb-2">
-        <nav className="flex justify-between items-end">
-          <img src={logo} className="translate-y-[40px] size-40" alt="" />
-          <ul className="flex gap-5 text-slate-700">
+      <div className="container mx-auto w-full sticky top-0 left-0 z-50 bg-white px-3 pb-2">
+        <nav className="flex justify-between items-end bg-white">
+          <img src={logo} className="translate-y-[30px] size-30" alt="" />
+
+          {/* desktop menu */}
+          <ul className="md:flex gap-5 text-slate-950 hidden">
             <li className="cursor-pointer">Home</li>
             <li className="cursor-pointer">About</li>
             <li className="cursor-pointer">Services</li>
             <li className="cursor-pointer">Contact</li>
           </ul>
+
+          {/* mobile menu */}
+          <div className="block md:hidden">
+            <IoMenu className="size-12" />
+          </div>
         </nav>
       </div>
     </>
