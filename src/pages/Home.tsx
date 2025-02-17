84256eb67d82bf6b5494cc57495232d468a6860c
Rafael Castro <ralphc1992@live.com>
2025-02-17 10:14:32 -0600

    changed fonts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,11 @@ export const Home: React.FC = () => {
       <div className="container mx-auto  top-0">
         <Slideshow />
         <div className="h-[3000px]">
-          <div className="bg-gradient-to-t from-slate-950 to-slate-700 shadow-md mx-auto w-[90%] max-w-[600px] translate-y-[-50%] py-10">
-            <h1 className="text-2xl text-white text-center">
+          <div className="px-3 bg-gradient-to-t from-slate-950 to-slate-700 shadow-md mx-auto w-[90%] max-w-[600px] translate-y-[-30%] py-10">
+            <h1 className="text-xl text-white text-center">
               LEAKED PROOF MANAGEMENT SERVICES
             </h1>
-            <p className="text-neutral-400 text-center">
+            <p className="text-sm text-neutral-400 text-center">
               Your trusted partner in real estate—connecting buyers, sellers,
               and investors for seamless deals and real opportunities.
             </p>
