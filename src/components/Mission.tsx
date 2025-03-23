6cc87d50871d0f41d963229fa68a3efdff5bad25
Rafael Castro <ralphc1992@live.com>
2025-03-22 21:49:52 -0500

    removed py

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -8,7 +8,7 @@ export const Mission: React.FC = () => {
     <>
       <div
         id="mission"
-        className="bg-gradient-to-t bg-white py-[50px] md:py-[100px]"
+        className="bg-gradient-to-t bg-white py-[30px] md:py-[100px]"
       >
         <h1 className="text-center text-2xl md:text-4xl text-white mb-5">
           Your Trusted Partner in Real Estate – Buying, Selling & Investing Made
