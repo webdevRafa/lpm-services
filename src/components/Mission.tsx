bd0b225d1dd91b2ff627ed3d14d14be2ff53fd77
Rafael Castro <ralphc1992@live.com>
2025-03-30 17:07:26 -0500

    removed home text

--- src/components/Mission.tsx
+++ src/components/Mission.tsx
@@ -10,46 +10,7 @@ export const Mission: React.FC = () => {
         id="mission"
         className=" bg-gradient-to-t bg-white py-[10px] md:py-[100px]"
       >
-        <div className="flex flex-col md:flex-row gap-5  space-around mx-auto max-w-[1400px] ">
-          <div className="w-full flex items-center justify-center">
-            <div className="px-3">
-              <h1 className="text-gray-900 text-center text-xl font-bold md:text-2xl lg:text-4xl mb-5">
-                At Leaked Proof Management Services, we are dedicated to guiding
-                you through every step of the real estate investing process with
-                clarity and confidence.
-              </h1>
-              <p className="text-left text-sm text-gray-800 mb-4">
-                Whether you're looking to buy or sell a property, we can help
-                you navigate the market, offering expert advice on properties in
-                any condition.
-              </p>
-              <p className="text-left text-sm text-gray-800 mb-4">
-                We work closely with licensed professionals including real
-                estate agents, attorneys, and financial advisors to ensure you
-                receive{" "}
-                <span className="font-bold text-gray-700">
-                  top-quality service at every stage.
-                </span>
-              </p>
-              <p className="text-left text-sm text-gray-800 mb-4">
-                Our mission is to{" "}
-                <span className="font-bold text-gray-700">
-                  educate and empower you
-                </span>
-                , providing a thorough understanding of the process so you can
-                make informed decisions.
-              </p>
-              <p className="text-left text-sm text-gray-800 mb-4">
-                Whether you're buying your first investment property or selling
-                one in any condition.{" "}
-                <span className="font-bold text-gray-700">
-                  We're here to support you every step of the way
-                </span>
-                , making your real estate journey a success.
-              </p>
-            </div>
-          </div>
-        </div>
+        <div className="flex flex-col md:flex-row gap-5  space-around mx-auto max-w-[1400px] "></div>
         <div className="w-full mt-10 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 px-10">
           {/* CARD 1 */}
           <div className="w-full rounded-lg overflow-hidden shadow-md relative group h-[500px]">
