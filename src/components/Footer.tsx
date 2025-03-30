a1299db275196ee72256353839d279f6b7f11565
Rafael Castro <ralphc1992@live.com>
2025-03-30 17:14:48 -0500

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
+import { ImYoutube2 } from "react-icons/im";
+
 export const Footer: React.FC = () => {
   return (
     <>
-      <div className="w-full py-20 bg-gray-900"></div>
+      <div className="w-full  bg-gray-900">
+        <div className="w-full flex items-center gap-5 justify-center p-5">
+          <div>
+            <h1 className="text-white">PLACES YOU CAN FIND ME..</h1>
+          </div>
+          <ImYoutube2 className="text-white size-40 hover:text-red-700 cursor-pointer transition ease-in-out duration-300" />
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,12 @@
 import { AboutUs } from "../components/AboutUs";
 import { Hero } from "../components/Hero";
-
+import { Footer } from "../components/Footer";
 export const About: React.FC = () => {
   return (
     <>
       <Hero />
       <AboutUs />
+      <Footer />
     </>
   );
 };
