ac816b034c8b7d49eb3fdf7d9e4d9651eeefec72
Rafael Castro <ralphc1992@live.com>
2025-05-02 15:47:31 -0500

    fixed navbar fr

--- src/pages/ViewListingsPage.tsx
+++ src/pages/ViewListingsPage.tsx
@@ -1,13 +1,12 @@
 import { Footer } from "../components/Footer"
 import { Listings } from "../components/Listings"
-import { Navbar } from "../Navbar"
 
 
 
 
 export const ViewListingsPage: React.FC = () => {
     return <>
-    <Navbar />
+    
     <Listings />
     <Footer />
     </>
